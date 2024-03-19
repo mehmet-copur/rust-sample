337f46721f42a70a51e16e5a666d38a404816fd5
Mehmet Copur <BHR_COPUR503@kpn.com>
2024-03-19 14:17:10 +0100

    bad code intentioanally to get samoe warnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
+    let unused_variable = 123;
+
+    let truth = true;
+    if truth == true {
+        println!("It's true!");
+    }
+
+    // This is not formatted according to Rust's standard style
+    let badly_formatted=vec![1,2,3];
+
     println!("Hello, world!");
+}
+
+fn unused_function() {
+    println!("This function is never used.");
+}
+
+#[allow(dead_code)]
+struct UnusedStruct {
+    a: i32,
 }
